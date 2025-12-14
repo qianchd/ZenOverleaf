8f608ef35c6c11245852f8edbbc822f2d307ceea
qianchd <qianchd@gmail.com>
2025-12-14 10:13:14 +0800

    switch to cloudfare cors proxy

--- content.js
+++ content.js
@@ -243,7 +243,7 @@
                 try {
                     remoteRefs = await git.listServerRefs({
                         http: git.http, url: repo, prefix: 'refs/heads',
-                        corsProxy: 'https://cors.isomorphic-git.org', onAuth: authCallback
+                        corsProxy: 'https://gitcors4516.qianchd.workers.dev', onAuth: authCallback
                     });
                 } catch (e) { console.warn("List refs warning:", e); }
 
@@ -262,7 +262,7 @@
                     if(!isAuto) updateStatus('Fetching...', '#007bff');
                     await git.fetch({
                         fs: FS, http: git.http, dir: dir, remote: 'origin', ref: branch,
-                        corsProxy: 'https://cors.isomorphic-git.org', onAuth: authCallback,
+                        corsProxy: 'https://gitcors4516.qianchd.workers.dev', onAuth: authCallback,
                         depth: 1, singleBranch: true
                     });
                     await git.checkout({ fs: FS, dir: dir, ref: branch, force: true });
@@ -377,7 +377,7 @@
                 if(!isAuto) updateStatus('Pushing...', '#007bff');
                 await git.push({
                     fs: FS, http: git.http, dir: dir, remote: 'origin', ref: branch,
-                    force: true, onAuth: authCallback, corsProxy: 'https://cors.isomorphic-git.org',
+                    force: true, onAuth: authCallback, corsProxy: 'https://gitcors4516.qianchd.workers.dev',
                 });
 
                 updateStatus(`Success: ${new Date().toLocaleTimeString()}`, '#28a745');
@@ -527,6 +527,38 @@
                 if (!document.querySelector('.loading-panel')) return resolve();
                 const timer = setInterval(() => {
                     if (!document.querySelector('.loading-panel')) {
+                        clearInterval(timer);
+                        resolve();
+                    }
+                }, 100);
+            });
+        }
+        function waitForLoadingGone() {
+            return new Promise(resolve => {
+                // check if loading panel is gone.
+                if (!document.querySelector('.loading-panel')) return resolve();
+                let counter = 0;
+                let counter_large = 0;
+                let counter_leak = 0;
+                const timer = setInterval(() => {
+                    const button_bold = document.querySelector("div.ol-cm-toolbar-button-group:nth-child(4)");
+                    const loader = document.querySelector('.loading-panel');
+                    const premiumBadges = document.querySelectorAll("#ol-cm-toolbar-wrapper > div.ol-cm-toolbar.toolbar-editor > div:nth-child(n+3):nth-child(-n+7)");
+                    if(premiumBadges.length > 0) {
+                        counter += 1;
+                    }
+                    if(premiumBadges.length > 2) {
+                        counter_large += 1;
+                    }
+                    counter_leak += 1;
+                    if ((!loader && button_bold !== null && button_bold.style.display === "" && (counter > 1 || premiumBadges.length > 2)) || counter_large > 8 || counter > 8) {
+                        clearInterval(timer);
+                        resolve();
+                    } else if (premiumBadges.length > 2) {
+                        counter = 0;
+                    }
+                    if(counter_leak > 50) {
+                        console.log("warning: leak");
                         clearInterval(timer);
                         resolve();
                     }
